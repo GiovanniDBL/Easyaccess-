b67eef0bfab36c8428d6fb63f73b6bfc1fd736f6
Giovanni Brito <auxdesarrollo.sisec@gmail.com>
2021-01-23 15:55:30 -0500

    versionnext

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,10 @@ import { MessageService } from './services/message.service';
 import { NgxCaptchaModule } from 'ngx-captcha';
 import { NgxWhastappButtonModule } from 'ngx-whatsapp-button';
 import { FuncionesComponent } from './components/funciones/funciones.component';
+import { ResidencialComponent } from './components/residencial/residencial.component';
+import { EscolarComponent } from './components/escolar/escolar.component';
+import { CorporativoComponent } from './components/corporativo/corporativo.component';
+import { ConstruccionComponent } from './components/construccion/construccion.component';
 
 
 
@@ -27,7 +31,11 @@ import { FuncionesComponent } from './components/funciones/funciones.component';
     NavbarComponent,
     HomeComponent,
     ContactoComponent,
-    FuncionesComponent
+    FuncionesComponent,
+    ResidencialComponent,
+    EscolarComponent,
+    CorporativoComponent,
+    ConstruccionComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,11 +3,19 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FuncionesComponent } from './components/funciones/funciones.component';
+import { ResidencialComponent } from './components/residencial/residencial.component';
+import { EscolarComponent } from './components/escolar/escolar.component';
+import { CorporativoComponent } from './components/corporativo/corporativo.component';
+import { ConstruccionComponent } from './components/construccion/construccion.component';
 
 const APP_ROUTES: Routes = [
     { path: 'inicio', component: HomeComponent },
     { path: 'funciones', component: FuncionesComponent },
     { path: 'contacto', component: ContactoComponent },
+    { path: 'residencial', component: ResidencialComponent },
+    { path: 'escolar', component: EscolarComponent },
+    { path: 'corporativo', component: CorporativoComponent },
+    { path: 'construccion', component: ConstruccionComponent },
     { path: '**', pathMatch:'full', redirectTo: 'inicio' }
 ];
 
